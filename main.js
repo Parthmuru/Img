58bfc84c01efd340f1b6470a0ca2a6d996c91048
Parthmuru <82873872+Parthmuru@users.noreply.github.com>
2021-12-15 15:52:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,23 +16,23 @@ function take_snapshot(){
 }
 console.log("ml5 version = " + ml5.version);
 
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/cejqx6bhZ/.model.json",modelLoaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/1gcYv3uST/model.json",modelLoaded);
 
 function modelLoaded(){
     console.log("Model Loaded")
 }
 
 function check(){
-    img = document.getElementById('capture_image');
+    img = document.getElementById('captured_image');
     classifier.classify(img, gotResult);
 }
 
-function gotResult(error, result){
+function gotResult(error, results){
     if(error){
     console.log(error);
     }else{
-    console.log(result);
-    document.getElementById("result1").innerHTML=results[0].label;
-    document.getElementById("result2").innerHTML=results[0].confidence.toFixed(3);
+    console.log(results);
+    document.getElementById("result_1").innerHTML=results[0].label;
+    document.getElementById("result_2").innerHTML=results[0].confidence.toFixed(3);
 }
 }
